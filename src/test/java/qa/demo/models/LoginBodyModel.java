de8e45af6c4e83c2417abf205fa4cda82931107f
egolikov <panda_93a@mail.ru>
2023-10-04 20:11:17 +0600

    * upd project

---
+++ src/test/java/qa/demo/models/LoginBodyModel.java
@@ -0,0 +1,9 @@
+package qa.demo.models;
+
+import lombok.Data;
+
+@Data
+public class LoginBodyModel {
+    String password;
+    String userName;
+}
---
+++ src/test/java/qa/demo/models/LoginResponseModel.java
@@ -0,0 +1,16 @@
+package qa.demo.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class LoginResponseModel {
+    String userId;
+    String username;
+    String password;
+    String token;
+    String expires;
+    @JsonProperty("created_date")
+    String createdDate;
+    boolean isActive;
+}
