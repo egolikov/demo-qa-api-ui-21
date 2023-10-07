1be3171d763a78bb68422672e19f267cf013ffb0
egolikov <panda_93a@mail.ru>
2023-10-07 16:05:13 +0600

    * upd project

--- src/test/java/qa/demo/pages/UserProfilePage.java
+++ src/test/java/qa/demo/pages/UserProfilePage.java
@@ -1,5 +1,6 @@
 package qa.demo.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.Cookie;
 
 import static com.codeborne.selenide.Condition.disappear;
@@ -8,6 +9,7 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class UserProfilePage {
 
+    @Step("Открытие страницы с полученными Cookies")
     public static void openUserProfileWithCookies(String userId, String token, String expires) {
         open("/favicon.ico");
         getWebDriver().manage().addCookie(new Cookie("userID", userId));
@@ -15,6 +17,7 @@ public class UserProfilePage {
         getWebDriver().manage().addCookie(new Cookie("expires", expires));
     }
 
+    @Step("Проверка отсутствия в списке удаленной книги")
     public static void checkDisappearBook(String bookId) {
         $("[id='" + bookId + "']").shouldBe(disappear);
     }
